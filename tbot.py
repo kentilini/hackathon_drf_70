0c0837defd2b0b0713d805f7b9fd0436191910fd
root <root@face.ogify.ru>
2016-11-20 12:48:04 +0300

    Use get instead of getitem.

--- tbot.py
+++ tbot.py
@@ -37,12 +37,12 @@ def search(message):
     if len(result) == 1:
         person_map = result[0]
         person = Person(person_map['name'],
-               person_map['link'],
-               person_map['knowledge'],
-               person_map['teaching_skills'],
-               person_map['in_person'],
-               person_map['how_easy'],
-               person_map['total'])
+               person_map.get('link', 'Not Provided'),
+               person_map.get('knowledge', 'Not Provided'),
+               person_map.get('teaching_skills', 'Not Provided'),
+               person_map.get('in_person', 'Not Provided'),
+               person_map.get('how_easy', 'Not Provided'),
+               person_map.get('total', 'Not Provided'))
         bot.send_message(message.chat.id, person.get_string())
         return
 
@@ -58,12 +58,13 @@ def handle_docs_photo(message):
         file_info = bot.get_file(message.photo[len(message.photo) - 1].file_id)
         attached_file = bot.download_file(file_info.file_path)
         path = str(uuid.uuid1())
-        f = open(str(uuid.uuid1()) + file_info.file_path, "wb")
+        f = open(path, "wb")
         f.write(attached_file)
         f.close()
 
         best_match = readfile.get_best_match(path)
-        person_map = webparser.get_prep_by_path(best_match['url'], best_match['name'])
+        print best_match
+        person_map = webparser.get_prep_by_path(best_match.get(u"url"), best_match.get(u"name"))
         person = Person(person_map['name'],
                         person_map['link'],
                         person_map['knowledge'],
@@ -78,6 +79,7 @@ def handle_docs_photo(message):
         logger.error(str(e) + " " + str(error_uuid))
         bot.reply_to(message, "К сожалению мне не удалось обработать ваш запрос по технической причине, " +
                      "вашей проблеме присовен уникальный идентификатор: " + str(error_uuid))
+
 
 def get_answer():
     return
