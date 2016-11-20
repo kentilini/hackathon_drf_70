f7035272de81decea27d4628e934fbcb19dfc1db
Morgen Matvey <melges.morgen@gmail.com>
2016-11-20 06:53:21 +0300

    Add search features to the chat bot face.

--- person.py
+++ person.py
@@ -1,12 +1,4 @@
 class Person:
-    name = "Овчинкин Владимир Александрович"
-    link = "http://wikimipt.org/wiki/%D0%9E%D0%B2%D1%87%D0%B8%D0%BD%D0%BA%D0%B8%D0%BD_%D0%92%D0%BB%D0%B0%D0%B4%D0%B8%D0%BC%D0%B8%D1%80_%D0%90%D0%BB%D0%B5%D0%BA%D1%81%D0%B0%D0%BD%D0%B4%D1%80%D0%BE%D0%B2%D0%B8%D1%87"
-    knowledge = 4.48
-    teaching_skills = 4.63
-    in_person = 4.09
-    how_easy = 3.52
-    total = 4.57
-
     def __init__(self, name, link, knowledge, teaching_skills, in_person, how_easy, total):
         self.name = name
         self.link = link
@@ -16,7 +8,7 @@ class Person:
         self.how_easy = how_easy
         self.total = total
 
-    def getString(self):
+    def get_string(self):
         return "Я считаю, что это " + self.name + \
                "\nЗнания: " + str(self.knowledge) + \
                "\nНавыки преподавания: " + str(self.teaching_skills) + \
--- tbot.py
+++ tbot.py
@@ -1,24 +1,35 @@
 import telebot
 import logging
-import urllib.request
+import uuid
 from person import Person
 from telebot import types
 
-API_TOKEN = '273095775:AAG0JALdAib4xobarKMge1e9DuaWJcmUXC4'
-HELP_MESSAGE = "Пришлите мне фотографию преподователя МФТИ и я попытаюсь определить кто это и дать информацию о нём"
+API_TOKEN = '277757979:AAEPrc9yqbSfZkQHti5q8Q0c8F0M-ttlbQs'
+HELP_MESSAGE = "Пришлите мне фотографию преподователя МФТИ и я попытаюсь определить кто это."
 
 bot = telebot.TeleBot(API_TOKEN)
 logger = telebot.logger
 telebot.logger.setLevel(logging.WARNING)
 
+
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
-    bot.reply_to(message, HELP_MESSAGE)
+    bot.send_message(message.chat.id, HELP_MESSAGE)
 
+
+@bot.inline_handler(func=lambda query: True)
+def send_welcome(query):
+    r = types.InlineQueryResultArticle('1', 'Result', types.InputTextMessageContent('Result message.'))
+    r2 = types.InlineQueryResultArticle('2', 'Result2', types.InputTextMessageContent('Result message2.'))
+    bot.answer_inline_query(query.id, [r, r2])
 
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
-    bot.reply_to(message, "Я не понимаю Вас.\n" + HELP_MESSAGE)
+    result = search_for(message.text)
+    markup = types.InlineKeyboardMarkup(3)
+    markup.row(types.InlineKeyboardButton(text="Найдено несколько подходящих записей.\nСмотреть все результаты",
+                                          switch_inline_query_current_chat=result.name))
+    bot.send_message(message.chat.id, result.get_string(), reply_markup=markup)
 
 
 @bot.message_handler(content_types=['photo'])
@@ -28,10 +39,13 @@ def handle_docs_photo(message):
         attached_file = bot.download_file(file_info.file_path)
 
         person = get_answer()
-        bot.reply_to(message, person.getString())
+        bot.reply_to(message, person.get_string())
 
     except Exception as e:
-        bot.reply_to(message, e)
+        error_uuid = uuid.uuid1()
+        logger.error(str(e) + " " + str(error_uuid))
+        bot.reply_to(message, "К сожалению мне не удалось обработать ваш запрос по технической причине, " +
+                     "вашей проблеме присовен уникальный идентификатор: " + str(error_uuid))
 
 def get_answer():
     return Person("Овчинкин Владимир Александрович",
@@ -42,5 +56,13 @@ def get_answer():
                     3.52,
                     4.57)
 
+def search_for(search_string):
+    return Person("Овчинкин Владимир Александрович",
+                  "http://wikimipt.org/wiki/%D0%9E%D0%B2%D1%87%D0%B8%D0%BD%D0%BA%D0%B8%D0%BD_%D0%92%D0%BB%D0%B0%D0%B4%D0%B8%D0%BC%D0%B8%D1%80_%D0%90%D0%BB%D0%B5%D0%BA%D1%81%D0%B0%D0%BD%D0%B4%D1%80%D0%BE%D0%B2%D0%B8%D1%87",
+                    4.48,
+                    4.63,
+                    4.09,
+                    3.52,
+                    4.57)
 
 bot.polling(none_stop=True, interval=3, timeout=3)
